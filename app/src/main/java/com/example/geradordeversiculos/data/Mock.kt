c2cef50800883211aea0958839b04581a791b249
Guilherme <guimaraesss02@gmail.com>
2023-08-24 01:31:00 -0300

    Atualização 3.0

--- app/src/main/java/com/example/geradordeversiculos/data/Mock.kt
+++ app/src/main/java/com/example/geradordeversiculos/data/Mock.kt
@@ -42,12 +42,39 @@ class Mock {
         Phrase("Não temas, porque eu sou contigo; não te assombres, porque eu sou o teu Deus; eu te fortaleço, e te ajudo, e te sustento com a minha destra fiel. Isaías 41:10",fortification, pt),
         //  ------------------------------------------- EM INGLÊS ------------------------------------------- //
         Phrase("Fear not, for I am with you; be not dismayed, for I am your God; I will strengthen you, I will help you, I will uphold you with my faithful right hand. Isaiah 41:10",fortification, en),
+        Phrase("I can all things that strengthen me. Philippians 4:13", fortification, en),
+        Phrase("God is our refuge and strength, a very present help in trouble. Psalm 46:1", fortification, en),
+        Phrase("The God who gives me strength and makes my way perfect. Psalm 18:32", fortification, en),
+        Phrase("For God has not given us a spirit of cowardice, but of power, of love and of a sound mind. 2 Timothy 1:7", fortification, en),
+        Phrase("Didn't I send you? Be strong and brave! fear not, nor be dismayed, for the Lord your God is with you wherever you go. Joshua 1:9", fortification, en),
+        Phrase("But those who hope in the Lord will renew their strength, they will mount up with wings like eagles, they will run and not be weary, they will walk and not be weary. Isaiah 40:31", fortification, en),
+        Phrase("The Lord is my strength and my shield; my heart trusted in him, and I was helped; therefore my heart leaps for joy, and with my song I will praise him. Psalm 28:7", fortification, en),
+        Phrase("Finally, my brothers, be strong in the Lord and in the power of his might. Ephesians 6:10", fortification, en),
+        Phrase("Vigiai, estai firmes na fé, portai-vos varonilmente e fortalecei-vos. 1 Coríntios 16:13", fortification, en),
 
-        Phrase("Esperança",hope, pt), // INSERIR VERSICULOS DE ESPERANÇA
 
+        Phrase("E o Deus da esperança vos encha de todo o gozo e paz no vosso crer, para que sejais ricos de esperança no poder do Espírito Santo. Romanos 15:13",hope, pt),
+        Phrase("Agora, pois, Senhor, que espero eu? A minha esperança está em ti. Salmos 39:7",hope, pt),
+        Phrase("Porque eu bem sei os pensamentos que tenho a vosso respeito, diz o Senhor; pensamentos de paz e não de mal, para vos dar o fim que esperais.Jeremias 29:11",hope, pt),
+        Phrase("Porque tu és a minha esperança, Senhor Deus; tu és a minha confiança desde a minha mocidade. Salmos 71:5",hope, pt),
+        Phrase("Mas os que esperam no Senhor renovarão as suas forças; subirão com asas como águias; correrão, e não se cansarão; andarão, e não se fatigarão. Isaías 40:31",hope, pt),
+        Phrase("A qual temos como âncora da alma, segura e firme, e que penetra até ao interior do véu. Hebreus 6:19",hope, pt),
+        Phrase("A minha alma espera somente no Senhor; porque dele vem a minha esperança. Salmos 130:5",hope, pt),
+        Phrase("E a esperança não traz confusão, porquanto o amor de Deus está derramado em nossos corações pelo Espírito Santo que nos foi dado. Romanos 5:5",hope, pt),
+        Phrase("As misericórdias do Senhor são a causa de não sermos consumidos, porque as suas misericórdias não têm fim. Lamentações 3:24",hope, pt),
+        Phrase("Aos quais Deus quis fazer conhecer quais são as riquezas da glória deste mistério entre os gentios, que é Cristo em vós, esperança da glória. Colossenses 1:27",hope, pt),
         //  ------------------------------------------- EM INGLÊS ------------------------------------------- //
+        Phrase("Now may the God of hope fill you with all joy and peace in believing, that you may abound in hope by the power of the Holy Spirit. Romans 15:13",hope, en),
+        Phrase("And now, Lord, what do I wait for? My hope is in You. Psalm 39:7",hope, en),
+        Phrase("For I know the thoughts that I think toward you, says the Lord, thoughts of peace and not of evil, to give you a future and a hope. Jeremiah 29:11",hope, en),
+        Phrase("For You are my hope, O Lord God; You are my trust from my youth. Psalm 71:5",hope, en),
+        Phrase("But those who wait on the Lord shall renew their strength; they shall mount up with wings like eagles, they shall run and not be weary, they shall walk and not faint. Isaiah 40:31",hope, en),
+        Phrase("This hope we have as an anchor of the soul, both sure and steadfast, and which enters the Presence behind the veil. Hebrews 6:19",hope, en),
+        Phrase("I wait for the Lord, my soul waits, and in His word I do hope. Psalm 130:5",hope, en),
+        Phrase("Now hope does not disappoint, because the love of God has been poured out in our hearts by the Holy Spirit who was given to us. Romans 5:5",hope, en),
+        Phrase("The Lord's lovingkindnesses indeed never cease, for His compassions never fail. Lamentations 3:24",hope, en),
+        Phrase("To them God willed to make known what are the riches of the glory of this mystery among the Gentiles: which is Christ in you, the hope of glory. Colossians 1:27",hope, en),
 
-        Phrase("teste em esperança",hope, en), // INSERIR VERSICULOS DE ESPERANÇA EM INGLES
 
         Phrase("Em tudo, dai graças, porque esta é a vontade de Deus em Cristo Jesus para convosco. 1 Tessalonicenses 5:18", gratitude, pt),
         Phrase("Entrai pelas portas dele com gratidão e em seus átrios com louvor; dai-lhe graças e bendizei o seu nome. Salmos 100:4", gratitude, pt),
@@ -67,7 +94,12 @@ class Mock {
         Phrase("Louvai ao Senhor! Louvai ao Senhor, porque ele é bom, e a sua benignidade dura para sempre. Salmos 106:1", gratitude, pt),
         Phrase("Dizendo: Amém. Louvor, e glória, e sabedoria, e ação de graças, e honra, e poder, e força ao nosso Deus, para todo o sempre. Amém. Apocalipse 7:12", gratitude, pt),
         //  ------------------------------------------- EM INGLÊS ------------------------------------------- //
-        Phrase("Praise the lord! Praise the Lord, for he is good, and his kindness endures forever. Psalm 106:1", gratitude, en), // INSERIR VERSICULOS EM INGLES
+        Phrase("Praise the lord! Praise the Lord, for he is good, and his kindness endures forever. Psalm 106:1", gratitude, en),
+        Phrase("In everything give thanks; for this is the will of God in Christ Jesus for you. 1 Thessalonians 5:18", gratitude, en),
+        Phrase("And let the peace of God rule in your hearts, to which also you were called in one body; and be thankful. Colossians 3:15", gratitude, en),
+        Phrase("Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. Philippians 4:6", gratitude, en),
+        Phrase("Let the message of Christ dwell among you richly as you teach and admonish one another with all wisdom through psalms, hymns, and songs from the Spirit, singing to God with gratitude in your hearts. Colossians 3:16", gratitude, en),
+        Phrase("Let us come before His presence with thanksgiving; let us shout joyfully to Him with psalms. Psalm 95:2", gratitude, en),
 
         Phrase("Porque o Senhor dá a sabedoria; da sua boca é que vem o conhecimento e o entendimento. Provérbios 2:6", wisdom, pt),
         Phrase("A sabedoria é a coisa principal; adquire, pois, a sabedoria; sim, com todos os teus bens, adquire o entendimento. Provérbios 4:7", wisdom, pt),
@@ -90,18 +122,42 @@ class Mock {
         Phrase("O sábio é mais poderoso do que o forte, e o homem de conhecimento do que o que tem força. Provérbios 24:5", wisdom, pt),
         Phrase("Mas a sabedoria que vem do alto é, primeiramente, pura, depois, pacífica, moderada, tratável, cheia de misericórdia e de bons frutos, sem parcialidade, e sem hipocrisia. Tiago 3:17", wisdom, pt),
         //  ------------------------------------------- EM INGLÊS ------------------------------------------- //
-
         Phrase("But the wisdom from above is first pure, then peaceable, gentle, and easy to be entreated, full of mercy and good fruits, without partiality, and without hypocrisy. James 3:17", wisdom, en),
-        // INSERIR EM INLÊS WISDOM
+        Phrase("For the Lord gives wisdom; from his mouth come knowledge and understanding. Proverbs 2:6", wisdom, en),
+        Phrase("The fear of the Lord is the beginning of wisdom, and the knowledge of the Holy One is understanding. Proverbs 9:10", wisdom, en),
+        Phrase("If any of you lacks wisdom, you should ask God, who gives generously to all without finding fault, and it will be given to you. James 1:5", wisdom, en),
+        Phrase("For wisdom is better than rubies, and all the things one may desire cannot be compared with her. Proverbs 8:11", wisdom, en),
+        Phrase("The wise in heart are called discerning, and gracious words promote instruction. Proverbs 16:21", wisdom, en),
 
 
-        Phrase("Coragem", courage, pt), // INSERIR VERSÍCULOS SOBRE CORAGEM
+        Phrase("Não to mandei eu? Esforça-te, e tem bom ânimo; não pasmes, nem te espantes; porque o Senhor teu Deus é contigo, por onde quer que andares. Josué 1:9", courage, pt),
+        Phrase("Espera pelo Senhor, tem bom ânimo, e fortifique-se o teu coração; espera, pois, pelo Senhor. Salmos 27:14", courage, pt),
+        Phrase("Esforçai-vos, e animai-vos; não temais, nem vos espanteis diante deles; porque o Senhor teu Deus é o que vai contigo; não te deixará nem te desamparará. Deuteronômio 31:6", courage, pt),
+        Phrase("Vigiai, estai firmes na fé; portai-vos varonilmente, e fortalecei-vos. 1 Coríntios 16:13", courage, pt),
+        Phrase("Esforçai-vos, e fortaleça-se o vosso coração, vós todos que esperais no Senhor. Salmos 31:24", courage, pt),
+        Phrase("Porque Deus não nos deu o espírito de temor, mas de fortaleza, e de amor, e de moderação. 2 Timóteo 1:7", courage, pt),
+        Phrase("Não temas, porque eu sou contigo; não te assombres, porque eu sou o teu Deus; eu te fortaleço, e te ajudo, e te sustento com a minha destra fiel. Isaías 41:10", courage, pt),
+        Phrase("Então eles, vendo a ousadia de Pedro e João, e informados de que eram homens sem letras e indoutos, maravilharam-se e reconheceram que eles haviam estado com Jesus. Atos 4:13", courage, pt),
+        Phrase("Tão somente esforça-te e tem mui bom ânimo, para teres o cuidado de fazer conforme a toda a lei que meu servo Moisés te ordenou; dela não te desvies, nem para a direita nem para a esquerda, para que prudentemente te conduzas por onde quer que andares. Josué 1:7", courage, pt),
+        Phrase("No dia em que eu temer, hei de confiar em ti. Salmos 56:3", courage, pt),
         //  ------------------------------------------- EM INGLÊS ------------------------------------------- //
-        Phrase("Teste em coragem", courage, en), // INSERIR VERSÍCULOS SOBRE CORAGEM EM INGLES
+        Phrase("Have I not commanded you? Be strong and of good courage; do not be afraid, nor be dismayed, for the Lord your God is with you wherever you go. Joshua 1:9", courage, en),
+        Phrase("Wait on the Lord; be of good courage, and He shall strengthen your heart; wait, I say, on the Lord! Psalm 27:14", courage, en),
+        Phrase("Be strong and of good courage, do not fear nor be afraid of them; for the Lord your God, He is the One who goes with you. He will not leave you nor forsake you. Deuteronomy 31:6", courage, en),
+        Phrase("Watch, stand fast in the faith, be brave, be strong. 1 Corinthians 16:13", courage, en),
+        Phrase("Wait on the Lord; be of good courage, and He shall strengthen your heart; wait, I say, on the Lord! Psalm 31:24", courage, en),
 
-        Phrase("Humildade", humility, pt), // INSERIR VERSÍCULOS SOBRE HUMILDADE
+        Phrase("Mas dá maior graça. Portanto, diz: Deus resiste aos soberbos, mas dá graça aos humildes. Tiago 4:6", humility, pt),
+        Phrase("O temor do Senhor é a instrução da sabedoria, e a humildade precede a honra. Provérbios 15:33", humility, pt),
+        Phrase("E o que a si mesmo se exaltar será humilhado; e o que a si mesmo se humilhar será exaltado. Mateus 23:12", humility, pt),
+        Phrase("Revesti-vos, pois, como eleitos de Deus, santos e amados, de entranhas de misericórdia, de benignidade, humildade, mansidão, longanimidade. Colossenses 3:12", humility, pt),
+        Phrase("Nada façais por contenda ou por vanglória, mas por humildade; cada um considere os outros superiores a si mesmo. Filipenses 2:3", humility, pt),
         //  ------------------------------------------- EM INGLÊS ------------------------------------------- //
-        Phrase("teste em humildade", humility, en), // INSERIR VERSÍCULOS SOBRE HUMILDADE EM INGLES
+        Phrase("But he gives us more grace. That is why Scripture says: God opposes the proud but shows favor to the humble. James 4:6", humility, en),
+        Phrase("The fear of the Lord teaches wisdom, and humility comes before honor. Proverbs 15:33", humility, en),
+        Phrase("For those who exalt themselves will be humbled, and those who humble themselves will be exalted. Matthew 23:12", humility, en),
+        Phrase("Therefore, as God's chosen people, holy and dearly loved, clothe yourselves with compassion, kindness, humility, gentleness, and patience. Colossians 3:12", humility, en),
+        Phrase("Do nothing out of selfish ambition or vain conceit. Rather, in humility value others above yourselves. Philippians 2:3", humility, en),
 
 
     )
